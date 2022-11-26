057bc460656f9488a2982fa2caf9075a6e5a2a33
Yuliya Dershevich <dershevitch.y@yandex.ru>
2022-11-26 19:37:42 +0300

    First commit: new tests

--- tests.py
+++ tests.py
@@ -22,3 +22,69 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_add_same_books(self):
+        # проверка, что нельзя добавить одну и ту же книгу дважды.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_rating_add_more_10(self):
+        # проверка, что нельзя выставить рейтинг больше 10
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_rating('Гордость и предубеждение и зомби',11)
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') == 1
+
+    def test_get_book_rating_book_not_list(self):
+        # Нельзя получить рейтинг книги, которой нет в списке.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert collector.get_book_rating('Война и мир') != 1
+
+    def test_get_books_with_specific_rating_value_5(self):
+        # Получаем список книг с рейтингом 5
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_rating('Гордость и предубеждение и зомби', 5)
+        collector.set_book_rating('Гарри Поттер', 5)
+        assert len(collector.get_books_with_specific_rating(5)) == 2
+
+    def test_add_book_in_favorites_book_in_list(self):
+        # Добавление книги в избранное.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
+        assert 'Что делать, если ваш кот хочет вас убить' in  collector.favorites
+
+    def test_add_book_in_favorites_book_not_books_rating(self):
+        # Нельзя добавить книгу в избранное, если её нет в словаре books_rating
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_book_in_favorites('Гарри Поттер')
+        assert 'Гарри Поттер' not in collector.favorites
+    def test_delete_book_from_favorites_book_in_list(self):
+        # удаление книги из избранного.
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
+        collector.delete_book_from_favorites('Что делать, если ваш кот хочет вас убить')
+        assert 'Что делать, если ваш кот хочет вас убить' not in collector.favorites
+
+    def test_get_list_of_favorites_books_two_books(self):
+        # получить список избранных книг
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+
+
